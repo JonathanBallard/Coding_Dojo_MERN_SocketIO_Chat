35f649daf856fb7e05fd7ad73e9455b0c53902c9
Jonathan Ballard <asgardschosen@gmail.com>
2020-11-21 22:38:39 -0800

    remembering React!

--- server.js
+++ server.js
@@ -6,21 +6,6 @@ const port = 8000;
 app.use( express.json());
 app.use( express.urlencoded({ extended: true }) );
 
-app.post("/", (req, res) => {
-    console.log(req);
-    console.log(req[0]);
-    console.log(req.username);
-    console.log('------------TESTING------------ ');
-    res.json({ username: req });
-})
-
-app.post("/name", (req, res) => {
-    console.log(req);
-    console.log(req[0]);
-    console.log(req.username);
-    console.log('------------NOPE------------ ');
-    res.json({ username: req });
-})
 
 //initializes express server to listen on port 8000 and send a message as soon as the server is ready
 const server = app.listen(port, () => {
--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -3,14 +3,28 @@ import './welcome.css';
 
 const Welcome = props => {
 
+    let [ username, setUsername ] = useState('anonymous');
+
+    const [welcomeStyling, setWelcomeStyling] = useState('welcome');
+
+    const setName = (name) => {
+        setUsername(name);
+    }
+
+    const openChat = () => {
+        console.log('hide welcome, open chat')
+        let newClasses = [...welcomeStyling, 'hidden'];
+        newClasses.join(' ');
+        setWelcomeStyling(newClasses);
+    }
 
     return (
-        <div className='welcome'>
+        <div className={welcomeStyling}>
             <h4>Get started right now!</h4>
             <h6>I want to start chatting with the name...</h6>
-            <form action="/name">
-                <input name="username" type="text" placeholder="My name..." />
-                <input className="submitBtn" type="submit" value="Start Chatting"/>
+            <form>
+                <input onChange={ setName } name="username" type="text" placeholder={ username } />
+                <input onClick={ openChat } className="submitBtn" type="submit" value="Start Chatting"/>
             </form>
         </div>
     )
